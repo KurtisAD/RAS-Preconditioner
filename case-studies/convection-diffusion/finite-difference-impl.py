97b453e5a9567bc611f01ed9fc94a5b1903cb53f
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2021-04-29 17:19:22 -0400

    Fix bug not allowing higher mesh sizes
    Arbitrary mesh sizes now supported.

--- case-studies/convection-diffusion/finite-difference-impl.py
+++ case-studies/convection-diffusion/finite-difference-impl.py
@@ -15,34 +15,38 @@ m = 1/N  # temporal step size
 U = []
 
 # Initial condition (at t = 0)
-U0 = np.ones((M-1)*(M-1))
+U0 = np.zeros((M-1)*(M-1))
+U0[:M-1] = 1.0
 U.append(U0)
 
 # Construct the finite difference matrix A (block tridiagonal)
 alpha = -4/h**2 - b1/h - b2/h
-beta = 1/h**2 + b1/h
-gamma = 1/h**2 + b2/h
+delta = 1/h**2
+beta = delta + b1/h
+gamma = delta + b2/h
 
 B1 = np.zeros((M-1,))
 B1[:2] = (alpha, gamma)
 B2 = np.zeros((M-1,))
-B2[:2] = (alpha, 1/h**2)
+B2[:2] = (alpha, delta)
 
 B = scipy.linalg.toeplitz(B1, B2)
 
-I_gamma = np.identity(M-1) * 1/h**2
+I_delta = np.identity(M-1) * delta
 I_beta = np.identity(M-1) * beta
 
-lower = scipy.linalg.toeplitz((0, 1, 0), (0, 0, 0))
-upper = scipy.linalg.toeplitz((0, 0, 0), (0, 1, 0))
+tri_vec = np.zeros(M-1)
+tri_vec[1] = 1
+lower = scipy.linalg.toeplitz(tri_vec, np.zeros(M-1))
+upper = scipy.linalg.toeplitz(np.zeros(M-1), tri_vec)
 
-A = np.kron(lower, I_gamma) + np.kron(upper, I_beta) + np.kron(np.identity(M-1), B)
+A = np.kron(lower, I_delta) + np.kron(upper, I_beta) + np.kron(np.identity(M-1), B)
 
 # At this point, we would precondition A, and then use GMRES
 # For now, using standard solver for illustration
 # System we want to solve is (I + mA)U^{n+1} = U^{n}
-for n in range(1, N+1):
-    Unp1 = scipy.linalg.solve((np.identity((M-1)**2) + m*A), U[n-1])
+for n in range(N):
+    Unp1 = scipy.linalg.solve((np.identity((M-1)**2) + m*A), U[n])
     U.append(Unp1)
 
 # U now contains the spatial approximations at each time step
