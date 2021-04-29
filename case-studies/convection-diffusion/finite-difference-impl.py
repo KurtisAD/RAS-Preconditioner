62b1fda7b81def93e1ce89df805b1ed2ba25a837
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2021-04-29 19:57:55 -0400

    Change sign for diffusion term and create plots
    The paper has a negative sign in front of the diffusion term for the equation, but I don't think this makes sense. Maybe it depends on the context - negative diffusion does seem to be possible. For our purposes, I've changed it to positive to correspond to the case of dye transport. Also added code for creating plots for a given timestep. It can be clearly seen that the dye diffuses and undergoes convection with the fluid.

--- case-studies/convection-diffusion/finite-difference-impl.py
+++ case-studies/convection-diffusion/finite-difference-impl.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.linalg
+import matplotlib.pyplot as plt
 
 # Advection coefficients, change to 0 for Poisson case
 b1 = 10
@@ -7,7 +8,7 @@ b2 = 20
 
 # Discretization parameters
 M = 64  # M x M mesh size
-N = 10 # number of time steps
+N = 10  # number of time steps
 h = 1/M  # spatial mesh step size
 m = 1/N  # temporal step size
 
@@ -20,8 +21,8 @@ U0[:M-1] = 1.0
 U.append(U0)
 
 # Construct the finite difference matrix A (block tridiagonal)
-alpha = -4/h**2 - b1/h - b2/h
-delta = 1/h**2
+alpha = 4/h**2 - b1/h - b2/h
+delta = -1/h**2
 beta = delta + b1/h
 gamma = delta + b2/h
 
@@ -49,6 +50,14 @@ for n in range(N):
     Unp1 = scipy.linalg.solve((np.identity((M-1)**2) + m*A), U[n])
     U.append(Unp1)
 
-# U now contains the spatial approximations at each time step
-for i in range(N+1):
-    print(U[i])
+# U now contains the spatial approximations at each time step, create some plots at various time steps
+x = np.linspace(0, 1, num=M)
+y = np.linspace(0, 1, num=M)
+
+timestep = 8
+
+u_approx = U[timestep].reshape((M-1, M-1))
+
+plt.pcolormesh(x, y, u_approx)
+plt.colorbar()
+plt.show()
