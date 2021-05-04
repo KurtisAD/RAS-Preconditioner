f61123144bf8ef1ffc680bd443155d7b9e0fc1e6
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2021-05-03 21:23:59 -0400

    Add ASH and RASH preconditioners from Corey

--- precond.py
+++ precond.py
@@ -11,10 +11,12 @@ class gmres_counter(object):
     def __init__(self, disp=True):
         self._disp = disp
         self.niter = 0
-
     def __call__(self, rk=None):
         self.niter += 1
 
+counter = gmres_counter()
+
+# Selection sort which is used to order the nodes in some functions.
 
 def selection_sort(arr):
     for i in range(len(arr)):
@@ -23,21 +25,25 @@ def selection_sort(arr):
     return arr
 
 
+# The graph G which is associated to the matrix A.
+
 def graph(A):
     len_A = len(A)
     G = nx.Graph()
-    G.add_nodes_from(list(range(1, len_A + 1)))
+    G.add_nodes_from(list(range(1,len_A + 1)))
 
     for i in range(len_A):
         for j in range(len_A):
             if A[i][j] != 0:
-                G.add_edge(i + 1, j + 1)
+                G.add_edge(i+1, j+1)
     return G
 
 
+# Canonical partitioning of the unknowns for the problem.
+
 def partition_graph(A, N):
     len_A = len(A)
-    size = int(len_A / N)
+    size = int(len_A/N)
     arr = np.zeros((N, size))
     counter = 1
     for i in range(N):
@@ -47,8 +53,10 @@ def partition_graph(A, N):
     return arr
 
 
+# Given a delta overlapping partition, this returns the next delta overlapping partition.
+
 def next_overlap(G, prev, N):
-    arr = [0] * N
+    arr = [0]*N
     for i in range(N):
         copy = prev[i][:]
         size = len(prev[i])
@@ -66,11 +74,15 @@ def next_overlap(G, prev, N):
     return arr
 
 
+# Generates the delta overlap partition.
+
 def delta_overlap(G, prev, N, delta):
     for i in range(delta):
         prev = next_overlap(G, prev, N)
     return prev
 
+
+# The matrices R of the article.
 
 def R(A, W):
     len_A = len(A)
@@ -83,6 +95,8 @@ def R(A, W):
     return R
 
 
+# This returns the subblock of A_i which we wish to invert.
+
 def restrict(A, W):
     len_A = len(A)
     n = len(W)
@@ -90,25 +104,29 @@ def restrict(A, W):
     copy = np.zeros((n, n))
     counter = 0
     for i in W:
-        copy_rows[counter, :] = A[int(i - 1), :]
+        copy_rows[counter,:] = A[int(i-1),:]
         counter += 1
     counter = 0
     for i in W:
-        copy[:, counter] = copy_rows[:, int(i - 1)]
+        copy[:,counter] = copy_rows[:,int(i-1)]
         counter += 1
 
     return copy
 
 
+# This takes the inverted subblock of A_i obtained from the function restrict and replaces it where it originated.
+
 def enlargen(A, A_enlarge, N, W):
     len_A = len(A)
     matrix = np.zeros((len_A, len_A))
-    start = int(W[0] - 1)
+    start = int(W[0]-1)
     end = start + len(W) - 1
-    matrix[start:end + 1, start:end + 1] = A_enlarge
+    matrix[start:end+1, start:end+1] = A_enlarge
 
     return matrix
 
+
+# AS precond
 
 def AS(A, N, W):
     len_A = len(A)
@@ -122,6 +140,8 @@ def AS(A, N, W):
 
     return matrix
 
+
+# RAS precond
 
 def RAS(A, N, W, W_0):
     len_A = len(A)
@@ -137,4 +157,33 @@ def RAS(A, N, W, W_0):
     return matrix
 
 
-counter = gmres_counter()
+# ASH precond
+
+def ASH(A, N, W, W_0):
+    len_A = len(A)
+    matrix = np.zeros((len_A, len_A))
+    for i in range(N):
+        R_i_delta = R(A, W[i])
+        R_0 = R(A, W_0[i])
+        A_i = np.matmul(np.matmul(R_i_delta, A), R_i_delta)
+        A_i_inv = np.linalg.inv(restrict(A_i, W[i]))
+        A_i_inv = enlargen(A, A_i_inv, N, W[i])
+        matrix = matrix + np.matmul(np.matmul(R_i_delta, A_i_inv), R_0)
+
+    return matrix
+
+
+# RASH precond
+
+def RASH(A, N, W, W_0):
+    len_A = len(A)
+    matrix = np.zeros((len_A, len_A))
+    for i in range(N):
+        R_i_delta = R(A, W[i])
+        R_0 = R(A, W_0[i])
+        A_i = np.matmul(np.matmul(R_i_delta, A), R_i_delta)
+        A_i_inv = np.linalg.inv(restrict(A_i, W[i]))
+        A_i_inv = enlargen(A, A_i_inv, N, W[i])
+        matrix = matrix + np.matmul(np.matmul(R_0, A_i_inv), R_0)
+
+    return matrix
